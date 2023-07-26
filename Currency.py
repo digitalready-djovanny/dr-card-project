cd9d9f7b28cfa6b55c7816f1fd62633fe28630b1
digitalready-Sergio <140424833+digitalready-Sergio@users.noreply.github.com>
2023-07-26 14:31:20 -0400

    Add files via upload

---
+++ Currency.py
@@ -0,0 +1,17 @@
+#Sergio 
+#Currency 
+
+
+class Player():
+    def __init__ (self,name,hand,Chips=0, points=[]):
+        self.name=name
+        self.hand=hand
+        self.chips=Chips
+        self.points=points
+
+
+numberofpeople=1
+totalChips=250 
+print(totalChips)
+
+
