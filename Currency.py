685e889a84c9a10c2a0d5c49790f99a4d3527429
digitalready-Sergio <140424833+digitalready-Sergio@users.noreply.github.com>
2023-07-31 14:56:05 -0400

    Currency.py

--- Currency.py
+++ Currency.py
@@ -1,17 +1,16 @@
 #Sergio 
 #Currency 
 
-
 class Player():
-    def __init__ (self,name,hand,Chips=0, points=[]):
-        self.name=name
-        self.hand=hand
-        self.chips=Chips
-        self.points=points
-
+    def __init__ (Self,name,hand,chips=250,):
+        Self.name=name
+        Self.hand=hand
+        Self.chips=chips
+     
+dude=Player("dude", [])
+dude.chips += 250
+print(dude.chips)
 
 numberofpeople=1
-totalChips=250 
-print(totalChips)
 
 
