27a6c46a9620cb4da43cbb994bdf280f1224ea70
digitalready-Sergio <140424833+digitalready-Sergio@users.noreply.github.com>
2023-08-07 14:58:10 -0400

    Currency.py

--- Currency.py
+++ Currency.py
@@ -1,20 +1,27 @@
 #Sergio 
 #Currency 
-
+import hand 
 class Player():
     def __init__ (Self,name,hand,chips=250,):
         Self.name=name
         Self.hand=hand
         Self.chips=chips
-     
+
+
+
+
+    def win_bet(self, bet):
+        
+        dude.chips+=100
+        print("You've Won"+str(100)+"Chips")
+    def lose_bet(self, bet):
+        
+        dude.chips-=100 
+        print("You've lost"+str(100)-"Chips")
 dude=Player("dude", [])
 dude.chips +=250 
 dude.chips-=100
 dude.chips=400
 print(dude.chips)
-print("You Have"+str(400)+"Chips")
-
-
-
-numberofpeople=1
+print("You Have" +str(400)+ "Chips")
  
