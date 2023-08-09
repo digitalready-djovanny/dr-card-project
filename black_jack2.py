c9f0005781e6f54b2b4470776c475a430ca08093
digitalready-djovanny <140424788+digitalready-djovanny@users.noreply.github.com>
2023-08-09 14:50:55 -0400

    Update black_jack2.py

--- black_jack2.py
+++ black_jack2.py
@@ -1,6 +1,7 @@
 import random
 from card import Card
 
+stop = 0
 
 deck = Card.new_deck()
 
@@ -18,96 +19,79 @@ class Player():
 
 Bob = Player("Bob", [], 100 )
 
-class Computer():
-    def __init__ (self,computer,hand=[0],money=0):
-        self.computer = computer  
-        self.hand = hand
-        self.money = money
-Computer = Computer("Computer")
+computer = Player("computer", [], 100)
 
 def new_card():
     card = deck.pop()
     return card
 
 def sum_Player():
-    sum = 0
+    total = 0
     for card in Bob.hand:
-        sum+= card.value
-    return sum
+        total += card.value
+    return total
+
+def sum_computer():
+    total = 0
+    for card in computer.hand:
+        total += card.value
+    return total
 
 
+def check_win():
+    if sum_Player() >21:
+        return print("You such dumb stubid u bust")
+ 
+def computer_hand():
+    while sum_computer() < 21:
+
+        computer.hand.append(deck.pop())
+        
+    computer.hand.pop()
+    return sum_computer()
 
 def black_jack():
-    Bob.hand.append(new_card())
-    print(f"{Bob.hand}, {sum_Player()}, You have {Bob.money} dollars worth of chips.")
-
-    if player_input() == "hit":
         Bob.hand.append(new_card())
-        print(f"{Bob.hand}, {sum_Player()}, You have {Bob.money} dollars worth of chips.")
+        Bob.hand.append(new_card())
+        if sum_Player() > 21:
+            return print(f"Your hand is {Bob.hand}, {sum_Player()}, You have {Bob.money} dollars worth of chips.")
 
-    else:
-        return sum_Player()
+        print(f"You bust, {Bob.hand}, {sum_Player()}, You have {Bob.money} dollars worth of chips.")
 
+
+        while player_input() != "stop":
+
+            Bob.hand.append(new_card())
+            print(f"{Bob.hand}, {sum_Player()}, You have {Bob.money} dollars worth of chips.")
+            if sum_Player() > 21:
+                check_win()
+                break
+        
+        if sum_Player() <21 or sum_Player() == 21:
+
+            computer_hand()
+            print(f"The dealer's cards are {computer.hand} and the value of his hand is {sum_computer()}.")
+
+
+            if sum_Player() < sum_computer():
+                print("You lose")
+            else:
+                print("You win")
+    
+                
+        
+            
+    
 
 if __name__ == "__main__":
     black_jack()
 
 
-'''def black_jack():
-    
-    #first card that the player is given
-    player_hand = 0 
-    deck = Card.new_deck()
-    card = deck.pop()
-    player_hand += card.value
-    computer_hand = random.randint(2,21)
-    print(f' you drew: {card}, your hand is {player_hand}')
-    
-    #player starts playing
-    while not player_hand >21:
-        
-        if player_input()== "hit":
-            card=deck.pop()
-            player_hand += card.value
-            print(f'you drew {card}, your hand is {player_hand}')
 
 
-        elif player_input()== "stop":
-            if player_hand > computer_hand:
-                print("you win, we will need your right eye")
-            elif player_hand == computer_hand:
-                print("tie, we will be taking your left foot")
-            else:
-                print("you lose, we will be taking your wife and children, thank you come again")
-    
-    if player_hand ==21:
-            print("you win, you only need to sell 1 of your kidneys now")
-    if player_hand >21:
-            print("bust, you lose, we taking 2 generations of your children, your left kidney , your wife, and your 2nd cousin from your mother's side")
-
-    '''
-
-'''def player_input():
-    x = ""
-    while x!= "hit" and x!= "stop":
-        x = input("Do you want to hit or stop, dont matter u going to lose anyway stooooooobid: ")
-    return x
-         
-         '''
-        
-            
-            
-
-    
-
-'''def hit():
-    deck = Card.new_deck()
-    draw_card = deck.pop()
-    return (draw_card.suit, draw_card.value)
 
 
-black_jack()
-'''
+
 
 
 
